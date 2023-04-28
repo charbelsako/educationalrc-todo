eed304f7fb824b9488476c3680f4f0b48988b6b6
Sharbel Sarkis <charbelsako@gmail.com>
2023-04-28 17:09:06 +0000

    build files

--- index.js
+++ index.js
@@ -49,5 +49,16 @@ const userRouter = require('./routes/user')
 app.use('/todo', todoRouter)
 app.use('/user', userRouter)
 
+// For heroku
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  console.log('production mode')
+  app.use(express.static('client/build'))
+  // console.log(path.resolve(__dirname, "client/", "build/", "index.html"))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/', 'build/', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
